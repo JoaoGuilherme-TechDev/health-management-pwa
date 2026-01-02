f754f8110a4591dbe41cf84841b5127aabda022f
JoaoGuilherme-TechDev <jguilherme.martins@outlook.com>
2026-01-02 14:31:26 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import ClientLayout from "./clientLayout"
-import { useEffect } from "react"
+
 
 const geist = Geist({ 
   subsets: ["latin"],
