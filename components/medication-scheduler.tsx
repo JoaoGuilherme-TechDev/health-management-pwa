62878934a9041f8948006c1f542ef2f07c40f1f1
JoaoGuilherme-TechDev <jguilherme.martins@outlook.com>
2026-01-07 13:26:00 -0300

    a

--- components/medication-scheduler.tsx
+++ components/medication-scheduler.tsx
@@ -41,13 +41,6 @@ export function MedicationScheduler() {
         return
       }
 
-      // Check each medication
-      // We use Promise.allSettled to ensure one failure doesn't stop others
-      await Promise.allSettled(
-        medications.map(med => 
-          pushNotifications.sendNewMedicationSchedule(user.id, med.name, med.id)
-        )
-      )
     }
 
     // Run immediately on mount
