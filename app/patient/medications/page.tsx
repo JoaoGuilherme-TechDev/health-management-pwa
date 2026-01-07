3d80ed548b7ae95a27cb2fb64057486c1ea9d280
JoaoGuilherme-TechDev <jguilherme.martins@outlook.com>
2026-01-07 16:28:26 -0300

    a

--- app/patient/medications/page.tsx
+++ app/patient/medications/page.tsx
@@ -10,8 +10,6 @@ import { useSearchParams, useRouter } from "next/navigation"
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 
-
-
 interface MedicationSchedule {
   id: string
   scheduled_time: string
