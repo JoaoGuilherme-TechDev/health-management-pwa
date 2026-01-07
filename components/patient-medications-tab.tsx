9544df82496361edb14668139d013d684c5cc9e3
JoaoGuilherme-TechDev <jguilherme.martins@outlook.com>
2026-01-07 09:20:19 -0300

    finally ?

--- components/patient-medications-tab.tsx
+++ components/patient-medications-tab.tsx
@@ -232,10 +232,7 @@ export function PatientMedicationsTab({ patientId }: { patientId: string }) {
       }
 
       await pushNotifications.sendNewMedication(patientId, formData.name)
-    
 
-     
-      
       
       alert("Medicamento e horários adicionados com sucesso!")
 
