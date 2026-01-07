3fc5f92d7c476b974a032c85291541135b9cf098
JoaoGuilherme-TechDev <jguilherme.martins@outlook.com>
2026-01-06 21:45:26 -0300

    a

--- components/patient-medications-tab.tsx
+++ components/patient-medications-tab.tsx
@@ -233,13 +233,6 @@ export function PatientMedicationsTab({ patientId }: { patientId: string }) {
 
       await pushNotifications.sendNewMedication(patientId, formData.name)
     
-
-      if (medication) {
-        await pushNotifications.sendNewMedicationSchedule(patientId, formData.name, medication.id)
-      }
-     
-      
-      
       alert("Medicamento e horários adicionados com sucesso!")
 
       setShowDialog(false)
